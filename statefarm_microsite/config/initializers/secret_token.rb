5c61e258609cd811bf93be95e620a82235e1dac2
Tony Seing <tony.seing@gmail.com>
2011-06-20 13:20:57 -0400

    initial commit

---
+++ statefarm_microsite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatefarmMicrosite::Application.config.secret_token = 'a7afde9ed76e7f850fb3f4f6ac7271638756bd7b921193b7a7cfd77d729104ae62e84f2a4d43148fc8cc9879fd12588e3939f65b55b58b0a4369c533e4b9fab4'
