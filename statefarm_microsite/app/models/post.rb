42e6f6884da0bea4324d7a308dd5d3297fdca6f3
Tony Seing <tony.seing@gmail.com>
2011-06-23 17:53:53 -0400

    added some validations

--- statefarm_microsite/app/models/post.rb
+++ statefarm_microsite/app/models/post.rb
@@ -7,4 +7,7 @@ class Post < ActiveRecord::Base
 	  }
     has_attached_file :video
     has_attached_file :asset
+    validates :title, :presence => true
+    validates :title, :length => {:minimum => 0, :maximum => 500}
+    validates :description, :presence => true
 end
