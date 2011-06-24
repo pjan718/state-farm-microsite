4c153b213e38375c45f4ba0246325c5c447f8361
Tony Seing <tony.seing@gmail.com>
2011-06-24 15:39:16 -0400

    asset upload/download fixed

--- statefarm_microsite/app/models/admin.rb
+++ statefarm_microsite/app/models/admin.rb
@@ -7,6 +7,10 @@ class Admin < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
+  VALID_EMAILS = %w(ddb.com ny.ddb.com)
   
-  
+protected
+  def valid_email_domain(email_address)
+    (email_address =~ /#{INVALID_EMAILS.map{|a| Regexp.quote(a)}.join("|")}/)
+  end
 end
