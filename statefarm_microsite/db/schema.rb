65fa39c8c0ec8b92f71647be0f94ffc0217b7ec6
Tony Seing <tony.seing@gmail.com>
2011-06-28 15:29:16 -0400

    more integration

--- statefarm_microsite/db/schema.rb
+++ statefarm_microsite/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110623194257) do
+ActiveRecord::Schema.define(:version => 20110628185810) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20110623194257) do
     t.string   "asset_content_type"
     t.integer  "asset_file_size"
     t.datetime "asset_updated_at"
+    t.text     "videolink"
   end
 
   create_table "users", :force => true do |t|
